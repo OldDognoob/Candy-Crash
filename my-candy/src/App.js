3fb5425b4906682f5484bb983c53cb0d7b9057b9
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-11-10 21:17:52 +0200

    wip-project

--- my-candy/src/App.js
+++ my-candy/src/App.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 /*Creating a board for the game by using 8 as width and 8 as height.
 In order to play we need 64 different colors. 
 The board for the game will be an array of 64 for items of different colors*/
@@ -11,8 +13,14 @@ const candyColors = [
   'red',
   'purple'
 ]
+// use setCurrentColorArrangement and instead of consoling outside of the loop 
+// we will set the setCurrentColorArrangement after the loop ending
+// so we will get the randomColorArrangement array after the loop is running to fill up with the 64 random items
 
 const App = () => {
+  //useState to set up in an empty array
+  const[currentColorArrangement, setCurrentColorArrangement]= useState([]);
+
   const createBoard = () => {
     // each time we get a random color, we need to be above the loop because every time we are looping we will get the random color arrangement 
     const randomColorArrangement = []
@@ -22,12 +30,15 @@ const App = () => {
       //we are speeding up a different number of colors
       //so to get a random color passing a random number from 0 to five to candyColors array and then return either yellow, red, orange or purple
         const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
-        randomColorArrangement.push(randomColor)
-
+        randomColorArrangement.push(randomColorArrangement)
     }
-    console.log(randomColorArrangement);
+    setCurrentColorArrangement(randomColorArrangement)
+   
   }
   createBoard();
+
+  console.log(currentColorArrangement);
+  
   return (
     <div></div>
   )
