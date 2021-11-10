837696e11602f9da2ebadbd1ccfa02d9d9042ab6
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-11-10 20:44:12 +0200

    wip-app-file

--- my-candy/src/App.js
+++ my-candy/src/App.js
@@ -1,7 +1,20 @@
-function App() {
-  return (
-    <div>Hello There!</div>
-  );
-}
+/*Creating a board for the game by using 8 as width and 8 as height.
+In order to play we need 64 different colors. 
+The board for the game will be an array of 64 for items of different colors*/
 
+const width = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'yellow',
+  'red',
+  'purple'
+]
+
+const App = () => {
+  return (
+    <div>Hello There! We are back !!!</div>
+  )
+}
 export default App;
